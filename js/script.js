25fd26ce7814eb0c338a1848c085831259002430
Leo <lregala.b@gmail.com>
2022-07-08 00:42:00 -0600

    height JS test

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
  "use strict";
  
 
-//  window.addEventListener('resize', ()=>{
-//     let vh = window.innerHeight*0.01;
-//     document.documentElement.style.setProperty('--vh', `${vh}px`);
-//  });
+ window.addEventListener('resize', ()=>{
+    let vh = window.innerHeight*0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+ });
 
 
 
