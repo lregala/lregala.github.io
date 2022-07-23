c6ff1522c81a72c20e3d9463138e6cdcf8674881
Leo <lregala.b@gmail.com>
2022-07-22 20:27:46 -0600

    redesign

--- js/script.js
+++ js/script.js
@@ -22,26 +22,10 @@ let mybutton = document.getElementById("myBtn"); //Get the button:
  }
 
 
- const faders = document.querySelectorAll('.fade-in');
- const appearOptions = {
-     threshold: 0.5,
-     // rootMargin: "0px 0px -10px 0px"
- };
-
- const appearOnScroll = new IntersectionObserver(
-     function(entries, 
-             appearOnScroll
-         ){
-             entries.forEach(entry => {
-             if (!entry.isIntersecting){
-                 return;
-             } else {
-                 entry.target.classList.add('appear');
-                 appearOnScroll.unobserve(entry.target);
-             }
-             });
-         },
-     appearOptions);
- faders.forEach(fader => {
- appearOnScroll.observe(fader);
+ window.addEventListener('resize', (event) => {
+    var AR = window.innerHeight/window.innerWidth;
+    var linearWidth = window.innerWidth+window.innerHeight;
+    var perimeter = window.innerWidth+window.innerHeight+window.innerWidth+window.innerHeight;
+    console.log(AR);
+    
 });
